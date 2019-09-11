ff74f43e82208a1e719946b5c4b5eb91e82abee9
yann300 <yann.levreau@gmail.com>
2019-09-11 15:18:22 +0200

    update prod workshop address

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   network: 'goerli',
-  address: '0x08a36b3ceef2862a070b55a1d55651bceab904a9'
+  address: '0xc5296CEf8a3ca94e8DAd5C3fC9605b5Ac6fCD297'
 };
