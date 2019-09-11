92a4ec56dfb57b332ac9b2f6b11f4c85cd304aa6
yann300 <yann.levreau@gmail.com>
2019-09-11 15:17:15 +0200

    update contract workshop address

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   network: 'goerli',
-  address: '0x08a36b3ceef2862a070b55a1d55651bceab904a9'
+  address: '0xc5296CEf8a3ca94e8DAd5C3fC9605b5Ac6fCD297'
 };
 
 /*
