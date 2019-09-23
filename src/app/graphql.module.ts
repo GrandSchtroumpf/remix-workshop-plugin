863de907adfcf61063c5db608ead71bf93570b4e
Grandschtroumpf <francois.guezengar@hotmail.fr>
2019-09-23 16:59:30 +0200

    fix bug with github import

--- src/app/graphql.module.ts
+++ src/app/graphql.module.ts
@@ -2,11 +2,9 @@ import {NgModule} from '@angular/core';
 import {ApolloModule, APOLLO_OPTIONS} from 'apollo-angular';
 import {HttpLinkModule, HttpLink} from 'apollo-angular-link-http';
 import {InMemoryCache, IntrospectionFragmentMatcher} from 'apollo-cache-inmemory';
-import { ApolloLink } from 'apollo-link';
+import { ApolloLink, concat, fromPromise } from 'apollo-link';
 import { introspectionQueryResultData } from '../assets/fragments-type';
 import { REMIX, RemixClient } from './remix-client';
-import { from } from 'rxjs';
-import { switchMap } from 'rxjs/operators';
 
 ////////////
 // SCHEMA //
@@ -22,27 +20,25 @@ function getAuthLink(remix: RemixClient) {
   return new ApolloLink((operation, forward) => {
     // Get the authentication token from local storage if it exists
     const tokenPromise = remix.call('settings', 'getGithubAccessToken');
-    // return forward(operation);
-    return from(tokenPromise).pipe(
-      switchMap(token => {
-        // Use the setContext method to set the HTTP headers.
-        const headers = { Authorization: `Bearer ${token}` };
-        operation.setContext({ headers });
-        return forward(operation); // Call the next link in the middleware chain.
-      })
-    ) as any;
+    return fromPromise(tokenPromise)
+    .flatMap(token => {
+      // Use the setContext method to set the HTTP headers.
+      const headers = { Authorization: `Bearer ${token}` };
+      operation.setContext({ headers });
+      return forward(operation); // Call the next link in the middleware chain.
+    });
   });
 }
 
 /////////
 // URI //
 /////////
-const uri = 'https://api.github.com/graphql';
 export function createApollo(httpLink: HttpLink, remix: RemixClient) {
+  const http = httpLink.create({ uri: 'https://api.github.com/graphql' });
   const authLink = getAuthLink(remix);
   return {
-    link: authLink.concat(httpLink.create({uri})),
-    cache: new InMemoryCache({ fragmentMatcher }),
+    link: concat(authLink, http),
+    cache: new InMemoryCache({ fragmentMatcher })
   };
 }
 
